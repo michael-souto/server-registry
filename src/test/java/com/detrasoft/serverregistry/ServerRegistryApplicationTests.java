8604d9bf0937681d83ba3363d8b15932771f4117
Michael Souto <michaelsouto@outlook.com>
2023-05-07 18:21:47 -0300

    Commit of project

---
+++ src/test/java/com/detrasoft/serverregistry/ServerRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.detrasoft.serverregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
